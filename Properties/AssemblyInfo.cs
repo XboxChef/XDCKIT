88296238332af9cd86349e344a546be7e9c93b2b
Serenity <gilbertarroyo77@gmail.com>
2022-06-22 02:46:14 -0400

    update 3.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,16 +7,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("XDCKIT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Serenit's Lab")]
 [assembly: AssemblyProduct("XDCKIT Lib")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Serenit's Lab")]
+[assembly: AssemblyTrademark("Serenit's Lab")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ee2b6ead-7d2e-4723-ab63-6cd539a3666c")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
